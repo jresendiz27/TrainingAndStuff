842531d4c617a6b58848af288a20904acde6d445
Juan Resendiz <jresendiz27@gmail.com>
2017-12-02 04:51:59 -0600

    Adding solution for sudoku problem
    Using sets and isolating rows, columns and squares
    
    Working on possible optimization.

---
+++ src/main/java/mx/jresendiz/training/codefights/sudoku2/Sudoku2.java
@@ -0,0 +1,49 @@
+package mx.jresendiz.training.codefights.sudoku2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Sudoku2 {
+    static boolean isValid(char[][] sudoku) {
+        assert sudoku.length == sudoku[0].length;
+
+        boolean isValid = true;
+        Set<Character> elements = new HashSet<>();
+        int sudokuSize = sudoku.length;
+
+        for (int i = 0; i < sudokuSize && isValid; i++) {
+            for (int j = 0; j < sudokuSize && isValid; j++) {
+                isValid = hasValidElements(sudoku[i][j], elements);
+            }
+            elements.clear();
+        }
+        for (int i = 0; i < sudokuSize && isValid; i++) {
+            for (int j = 0; j < sudokuSize && isValid; j++) {
+                isValid = hasValidElements(sudoku[j][i], elements);
+            }
+            elements.clear();
+        }
+
+        for (int i = 0; i < sudokuSize && isValid; i += 3) {
+            for (int j = i; j < 3 && isValid; j++) {
+                for (int k = i; k < 3 && isValid; k++) {
+                    isValid = hasValidElements(sudoku[j % 3][k % 3], elements);
+                }
+            }
+            elements.clear();
+        }
+
+        return isValid;
+    }
+
+    private static boolean hasValidElements(char c, Set<Character> elements) {
+        if (c == '.') {
+            return true;
+        }
+        if (!elements.contains(c)) {
+            elements.add(c);
+            return true;
+        }
+        return false;
+    }
+}
