dbf6946045ac18775e756e01de79ef371410baf2
Juan Resendiz <jresendiz27@gmail.com>
2017-12-02 04:53:15 -0600

    Addding comments for knowing what's going on

--- src/main/java/mx/jresendiz/training/codefights/sudoku2/Sudoku2.java
+++ src/main/java/mx/jresendiz/training/codefights/sudoku2/Sudoku2.java
@@ -10,20 +10,21 @@ public class Sudoku2 {
         boolean isValid = true;
         Set<Character> elements = new HashSet<>();
         int sudokuSize = sudoku.length;
-
+        // Checking rows
         for (int i = 0; i < sudokuSize && isValid; i++) {
             for (int j = 0; j < sudokuSize && isValid; j++) {
                 isValid = hasValidElements(sudoku[i][j], elements);
             }
             elements.clear();
         }
+        // Checkking columns
         for (int i = 0; i < sudokuSize && isValid; i++) {
             for (int j = 0; j < sudokuSize && isValid; j++) {
                 isValid = hasValidElements(sudoku[j][i], elements);
             }
             elements.clear();
         }
-
+        // Checking squares
         for (int i = 0; i < sudokuSize && isValid; i += 3) {
             for (int j = i; j < 3 && isValid; j++) {
                 for (int k = i; k < 3 && isValid; k++) {
