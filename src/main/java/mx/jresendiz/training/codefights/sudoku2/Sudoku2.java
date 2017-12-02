5faffe7a5e02c792a5d9d61f06b91a957e4b313d
Juan Resendiz <jresendiz27@gmail.com>
2017-12-02 11:32:02 -0600

    Fixing issues with validation process

--- src/main/java/mx/jresendiz/training/codefights/sudoku2/Sudoku2.java
+++ src/main/java/mx/jresendiz/training/codefights/sudoku2/Sudoku2.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Sudoku2 {
-    static boolean isValid(char[][] sudoku) {
+    static boolean sudoku2(char[][] sudoku) {
         assert sudoku.length == sudoku[0].length;
 
         boolean isValid = true;
@@ -16,9 +16,7 @@ public class Sudoku2 {
                 isValid = hasValidElements(sudoku[i][j], elements);
             }
             elements.clear();
-        }
-        // Checkking columns
-        for (int i = 0; i < sudokuSize && isValid; i++) {
+            // Checking columns
             for (int j = 0; j < sudokuSize && isValid; j++) {
                 isValid = hasValidElements(sudoku[j][i], elements);
             }
@@ -26,12 +24,16 @@ public class Sudoku2 {
         }
         // Checking squares
         for (int i = 0; i < sudokuSize && isValid; i += 3) {
-            for (int j = i; j < 3 && isValid; j++) {
-                for (int k = i; k < 3 && isValid; k++) {
-                    isValid = hasValidElements(sudoku[j % 3][k % 3], elements);
+            for (int j = 0; j < sudokuSize && isValid; j += 3) {
+                // validate square
+                for (int k = i; k < i + 3 && isValid; k++) {
+                    for (int l = j; l < j + 3 && isValid; l++) {
+                        isValid = hasValidElements(sudoku[k][l], elements);
+                    }
                 }
+                elements.clear();
             }
-            elements.clear();
+
         }
 
         return isValid;
