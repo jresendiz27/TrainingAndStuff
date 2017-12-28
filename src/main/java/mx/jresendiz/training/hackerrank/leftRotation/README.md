ebba30ab1b3c3f6a798a683abc1b01c3ce663e54
Juan Resendiz <jresendiz27@gmail.com>
2017-12-27 22:57:04 -0600

    Adding leftRotation spec and sample for hacker rank challenge

---
+++ src/main/java/mx/jresendiz/training/hackerrank/leftRotation/README.md
@@ -0,0 +1,3 @@
+Based in:[LeftRotation](https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem) at [https://hackerrank.com](HackerRank)
+
+![HackerRank challenge passing image](http://storage9.static.itmages.com/i/17/1228/h_1514436904_6165733_f4fd854558.png)
---
+++ src/test/groovy/mx/jresendiz/training/hackerrank/leftRotation/LeftRotationSpec.groovy
@@ -0,0 +1,15 @@
+package mx.jresendiz.training.hackerrank.leftRotation
+
+import spock.lang.Specification
+
+class LeftRotationSpec extends Specification {
+    void "Verify the left rotation problem"() {
+        // No more examples were added because they just provide you with one.
+        // Should work with all cases ... Screenshot is provided in the readme file
+        expect:
+        result == LeftRotation.rotate(numberOfRotation as int, elements as int[])
+        where:
+        numberOfRotation || elements        | result
+        4                || [1, 2, 3, 4, 5] | "5 1 2 3 4"
+    }
+}
