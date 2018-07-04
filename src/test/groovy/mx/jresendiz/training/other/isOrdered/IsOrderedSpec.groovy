97c11e01171a68ec52a289a3f9be2e40233207cb
Juan Reséndiz <jresendiz27@gmail.com>
2018-07-04 09:44:16 -0500

    Add new test case to IsOrderedSpec

--- src/test/groovy/mx/jresendiz/training/other/isOrdered/IsOrderedSpec.groovy
+++ src/test/groovy/mx/jresendiz/training/other/isOrdered/IsOrderedSpec.groovy
@@ -11,6 +11,7 @@ class IsOrderedSpec extends Specification {
         [1, 2, 3, 4, 5]          || true
         [5, 1, 2, 3, 4]          || false
         [1, 1, 1, 1, 1]          || true
+        [5, 4, 3, 2, 1]          || true
         [3, 2, 2, 4]             || false
         [1, 2, 4, 3, 5]          || false
         [1, 1, 1, 1, 1, 1, 0, 1] || false
